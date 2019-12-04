c86b9ae552a9d414eadfdabefad34e30280e5ad2
RachaelChar <charewicz.r@gmail.com>
2019-12-04 11:29:31 -0500

    require files and build basic employees object

--- Develop/app.js
+++ Develop/app.js
@@ -1,1 +1,13 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager.js");
+const Intern = require("./app/Intern.js");
+const Engineer = require("./app/Engineer.js");
+
+// object of Employees with properties of manager, intern, engineer (each an array that you push to when adding one)
+
+let employeesObject = {
+    managers: [],
+    engineers: [],
+    interns: []
+}
