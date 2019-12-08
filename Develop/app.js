a5a2b7160787fbfaa9ef2820c82388d1842b3d06
RachaelChar <charewicz.r@gmail.com>
2019-12-08 11:17:23 -0500

    chain employee type prompt to manager prompt

--- Develop/app.js
+++ Develop/app.js
@@ -34,9 +34,9 @@ function promptForManager() {
       type: "input",
       name: "email",
       message: "Enter the manager's email: ",
-      validate: function(email) {
+      validate: function (email) {
         return validator
-        .isEmail(email) || "Please enter a valid email."
+          .isEmail(email) || "Please enter a valid email."
       }
     }
     ]);
@@ -50,38 +50,104 @@ promptForManager()
     console.log(employeesObject);
     return answers;
   })
+  .then(function (answers) {
+    promptForEmployeeType(answers);
+    return answers;
+  })
   .catch(function (err) {
     console.log("catch", err);
   });
 
-  // promptForEmployeesType()
-  //   .then(function (answers) {
-  //     return answers;
-  //   })
+
+    //           .then(function (answers) {
+    // if (choices === "Intern") {
+    //   //loop back to first prompt again
+    //   return prompt('question 1 data');
+    // } else {
+    //   //ask second prompt
+    //   return prompt('question 2 data');
+    // }
+  // });
 
 
-
-
-
-
-function promptForEmployeesType() {
+function promptForEmployeeType() {
   return inquirer
     .prompt([{
       type: "list",
       name: "type",
-      message: "What kind of employee?",
+      message: "What kind of employee do you want to add to your team?",
       choices: ["Intern", "Engineer"]
     },
     ]);
 }
 
-function promptForEmployeeDetails() {
+
+function promptForInternDetails() {
   return inquirer
     .prompt([{
-      type: "list",
-      name: "type",
-      message: "What kind of employee?",
-      choices: ["Intern", "Engineer"]
+      type: "input",
+      name: "name",
+      message: "Enter the intern's name: ",
     },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter the intern's ID: ",
+      // source: https://searchcode.com/codesearch/view/98875892/
+      // using the "validate" property provided by inquirer, the following code checks that the id input is a number
+      validate: function validatetName(name) {
+        let valid = !isNaN(parseFloat(name));
+        return valid || "Please enter a number";
+      }
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter the intern's email: ",
+      validate: function (email) {
+        return validator
+          .isEmail(email) || "Please enter a valid email."
+      }
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter the intern's school: ",
+    }
+    ]);
+}
+
+function promptForEngineerDetails() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      name: "name",
+      message: "Enter the engineer's name: ",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter the engineer's ID: ",
+      // source: https://searchcode.com/codesearch/view/98875892/
+      // using the "validate" property provided by inquirer, the following code checks that the id input is a number
+      validate: function validatetName(name) {
+        let valid = !isNaN(parseFloat(name));
+        return valid || "Please enter a number";
+      }
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter the engineer's email: ",
+      validate: function (email) {
+        return validator
+          .isEmail(email) || "Please enter a valid email."
+      }
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter the engineer's GitHub username: ",
+    }
     ]);
 }
